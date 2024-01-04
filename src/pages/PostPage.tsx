5bc0b4140a5cde56e9b6c41366c76fe43b11bb58
Syed Afrid Ali <aliatif908@gmail.com>
2024-01-04 21:46:49 +0530

    feat: design Post page

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,5 +1,91 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
+import Actions from "../components/Actions";
+import Comment from "../components/Comment";
+import { useState } from "react";
+
 const PostPage = () => {
-  return <div>PostPage</div>;
+  const [liked, setLiked] = useState<boolean>(false);
+  return (
+    <>
+      <Flex>
+        <Flex gap={3} w={"full"} alignItems={"center"}>
+          <Avatar src="/zuck-avatar.png" size={"md"} name="Mark Zuckerberg" />
+          <Flex>
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              markzuckerberg
+            </Text>
+            <Image src="/verified.png" w={4} h={4} ml={4} />
+          </Flex>
+        </Flex>
+        <Flex gap={4} alignItems={"center"}>
+          <Text fontSize={"sm"} color={"gray.light"}>
+            1d
+          </Text>
+          <BsThreeDots />
+        </Flex>
+      </Flex>
+      <Text my={3}>Let's talk about Threads.</Text>
+      <Box
+        borderRadius={6}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"gray.light"}
+      >
+        <Image src="/post1.png" w={"full"} />
+      </Box>
+      <Flex gap={3} my={3}>
+        <Actions liked={liked} setLiked={setLiked} />
+      </Flex>
+      <Flex gap={2} alignItems={"center"}>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          238 replies
+        </Text>
+        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          {100 + (liked ? 1 : 0)} likes
+        </Text>
+      </Flex>
+      <Divider my={4} />
+      <Flex justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text fontSize={"2xl"}>👋</Text>
+          <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+        </Flex>
+        <Button>Get</Button>
+      </Flex>
+      <Divider my={4} />
+      <Comment
+        username="janedoe"
+        userAvatar="https://bit.ly/dan-abramov"
+        createdAt="2d"
+        likes={121}
+        comment="This looks great!"
+      />
+      <Comment
+        username="Kent Dodds"
+        userAvatar="https://bit.ly/kent-c-dodds"
+        createdAt="1d"
+        likes={12}
+        comment="Awesome!"
+      />
+      <Comment
+        username="janedoe"
+        userAvatar="https://bit.ly/prosper-baba"
+        createdAt="2d"
+        likes={121}
+        comment="This looks great!"
+      />
+    </>
+  );
 };
 
 export default PostPage;
